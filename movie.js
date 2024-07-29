2c97c3279cf935e6ca07ae3ad9e8b851dcac186b
play93 <95739833+play93@users.noreply.github.com>
2024-07-29 15:31:38 +0900

    영화카드 표시 및 검색구현

--- movie.js
+++ movie.js
@@ -7,7 +7,6 @@ const options = {
 	}
 };
 
-// 본인의 API 키를 넣어주셔야 합니다.
 const API_KEY = 'f30c30f3f27951a668326597b4239f06';
 const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko&page=1`;
 
@@ -16,14 +15,20 @@ fetch(URL)
 .then(data => {
   console.log(data);
   // 이후 데이터 처리
+  const movies = data.results;
+  const movieContainer = document.getElementsByClassName('movie_container')[0];
+  movies.forEach(movie => {
+    const card = mkCard(movie);
+    movieContainer.appendChild(card);
+  });
 })
 .catch(error => console.error('Error:', error));
 
 
 //영화 카드 생성 함수
-function mkCard(movie) {
+function mkCard(movie) {  
   
-  const card = document.creatElement('div');
+  const card = document.createElement('div');
   card.className = "movie_card"
 
   //평점(최대10)을 받아와 별(최대5개)로 표시 
@@ -46,24 +51,55 @@ function mkCard(movie) {
 
   //html생성
   card.innerHTML=`
-    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
-    <h3>${movie.title}</h3>
-    <p>평점: ${stars}<span>${movie.vote_average}</span></p>
-    <p>${movie.overview}</p>
+    <img class="movie_post" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+    <h3 class="movie_title">${movie.title}</h3>
+    <p class="movie_vote">평점: ${stars}<span>(${movie.vote_average})</span></p>
+    <p class="movie_description">${movie.overview}</p>
   `
-  card.addEventListner('click',function(){
+
+  //카드 클릭시 id를 alert로 출력
+  card.addEventListener('click',function(){
     alert(`영화의 ID: ${movie.id}`);
   })
-
+  return card;
 } 
-
-//카드 클릭시 id를 alert로 출력
 
 
 //검색구현
 //1. 인풋에 입력한 검색어를 받아옴
-//2. 입력값이 영화제목들 중에 있는지 없는지 확인
-//3. 
+
+//2. 검색버튼 클릭하면 입력값이 영화제목들 중에 있는지 없는지 확인
+/*
+  2-1 영화 카드들을 객체로 title과 overview만 넣어서
+  card[0] = {
+    title:
+    overview:
+  }
+  card[1] = {
+    title:
+    overview:
+  }
+  card[2] = {
+    title:
+    overview:
+  }
+  ...
+
+  2-2 객체를 돌며 입력값이 있는지 확인 (filter),,
+*/
+//=>활용해서 제목만 검색 제목+내용검색 만들 수 있을 것 같음
+
+//3. 해당하는 영화만 화면에 출력
+
+const searchKeyword = document.getElementById('search_keyword');
+const searchBtn = document.getElementById('search_btn');
+
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const val = searchKeyword.value;
+  console.log(val);
+})
+
 
 
 //API
